f87116a58026520ef4db71cf14ec687d98515f62
Sreecharanya Srinivasan <sree3charanya@gmail.com>
2021-02-25 20:27:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//Create an array with the following entry: 273.15.
+let  practiceFile = [273.5]
+
+//Use the push method to add the number 42 and the string "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
+
+practiceFile.push(42)
+practiceFile.push("hello");
+practiceFile.push(false, -4.6, "87");
+console.log("practiceFile[]:",practiceFile)
+
+//Use a single push to add the items false, -4.6, and “87” to the array.  Print the array to confirm the changes.
